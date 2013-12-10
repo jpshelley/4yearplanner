8c28aa5082f30aa00ccee237247dba826db2e6e0
Trevor Lund <tlund@iastate.edu>
2013-12-09 21:42:44 -0600

    Deleted uneeded code

--- web/index.php
+++ web/index.php
@@ -37,25 +37,6 @@ if (isset($_SESSION['netid']))
 
 }
 
-function addClass($semester, $classes)
-{
-	$select_sem = $database->prepare("SELECT semesterid FROM semester WHERE netid = ? AND order = ?");
-	$params = array($netid, $semester);
-	$select_sem->execute($params);
-	$semesterid = $select_sem->fetch();
-	$select_class = $database->prepare("SELECT courseid FROM course WHERE course_name = ?");
-	$insert_class = $database->prepare("INSERT INTO semester_schedule (courseid, semesterid) VALUES (?, ?)");
-	foreach($classes as $class)
-	{
-		$params = array($class);
-		$select_class->execute($params);
-		$courseid = $stmt_class->fetch();
-		
-		$params = array($courseid, $semesterid);
-		$insert_class->execute($params);
-	}
-}
-
 ?>
 <html lang="us">
     <head>
